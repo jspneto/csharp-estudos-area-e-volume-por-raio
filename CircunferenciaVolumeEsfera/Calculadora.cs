ac4397c3415b426aeaf2cc6a9f41e4be70905c82
José de Senna <josespnt1990@gmail.com>
2026-02-11 14:22:31 -0300

    refactor: torna estaticos os membros da classe Calculadora
    - Remove o objeto calc do programa principal

--- CircunferenciaVolumeEsfera/Calculadora.cs
+++ CircunferenciaVolumeEsfera/Calculadora.cs
@@ -4,14 +4,14 @@ namespace CircunferenciaVolumeEsfera;
 
 class Calculadora
 {
-    public double Pi = 3.14;
+    public static double Pi = 3.14;
 
-    public double Circunferencia(double raio)
+    public static double Circunferencia(double raio)
     {
         return 2 * Pi * raio;
     }
 
-    public double Volume(double raio)
+    public static double Volume(double raio)
     {
         return 4.0 / 3.0 * Pi * raio * raio * raio;
     }
--- CircunferenciaVolumeEsfera/Program.cs
+++ CircunferenciaVolumeEsfera/Program.cs
@@ -7,16 +7,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Calculadora calc = new Calculadora();
-
         Console.Write("Entre com o valor do raio: ");
         double raio = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
-        double circ = calc.Circunferencia(raio);
-        double volume = calc.Volume(raio);
+        double circ = Calculadora.Circunferencia(raio);
+        double volume = Calculadora.Volume(raio);
 
         Console.WriteLine($"Circunferência: {circ.ToString("F2", CultureInfo.InvariantCulture)}");
         Console.WriteLine($"Volume: {volume.ToString("F2", CultureInfo.InvariantCulture)}");
-        Console.WriteLine($"Valor de PI: {calc.Pi.ToString("F2", CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"Valor de PI: {Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture)}");
     }
 }
